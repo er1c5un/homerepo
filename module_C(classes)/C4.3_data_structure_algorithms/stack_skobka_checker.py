0de7c70b57bb74e74d4721a4f1af3d601f382bbb
eric <ericrad@bk.ru>
2022-12-20 20:11:40 +0300

    Модификация задачи балланса скобок под разные типы скобок

--- module_C(classes)/C4.3_data_structure_algorithms/stack_skobka_checker.py
+++ module_C(classes)/C4.3_data_structure_algorithms/stack_skobka_checker.py
@@ -1,17 +1,20 @@
 def par_checker(string):
+    dict_per = {')': '(', ']': '['}
     stack = []  # инициализируем стек
 
     for s in string:  # читаем строку посимвольно
-        if s == "(":  # если открывающая скобка,
+        if s in dict_per.values():  # если открывающая скобка,
             stack.append(s)  # добавляем её в стек
-        elif s == ")":
+        elif s in dict_per.keys():
             # если встретилась закрывающая скобка, то проверяем,
             # пуст ли стек и является ли верхний элемент открывающей скобкой
-            if len(stack) > 0 and stack[-1] == "(":
+            if len(stack) > 0 and stack[-1] == dict_per(s):
                 stack.pop()  # удаляем из стека
             else:  # иначе завершаем функцию с False
                 return False
     # если стек пустой, то незакрытых скобок не осталось
     # значит возвращаем True, иначе - False
     return len(stack) == 0
+
+
 
